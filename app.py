2b115cc88bc5ba86378579c81a2729da78bc166f
urich <56362549+bebeto55@users.noreply.github.com>
2026-06-18 16:05:41 +0200

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -3,16 +3,7 @@ import cv2
 import numpy as np
 import tempfile
 import av
-import streamlit as st
-import traceback
 
-try:
-    import cv2
-    st.success(f"OpenCV OK : {cv2.__version__}")
-
-except Exception:
-    st.code(traceback.format_exc())
-    st.stop()
 
 from ultralytics import YOLO
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase
